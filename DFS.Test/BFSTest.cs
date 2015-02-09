c7ee90b77b60aa93859ea543356a7072e9d9e7ee
Koki <u2daijibrother@gmail.com>
2015-02-10 05:54:57 +0900

    Add BFS algorithm and test.

---
+++ DFS.Test/BFSTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DFS.Test
+{
+    [TestFixture]
+    public class BFSTest
+    {
+        [Test]
+        public void Search_should_reach_each_node()
+        {
+            // Arrange
+            var target = new Node();
+            target.Adjacent = new Node[] { new Node(), new Node(){ Adjacent = new Node[]{ new Node(), new Node()} }, new Node() };
+
+            // Act
+            BFS.Search(target);
+
+            // Assert
+            AssertNode(target);
+        }
+
+        [Test]
+        public void Search_should_reach_each_node_()
+        {
+            // Arrange
+            var target = new Node();
+            target.Adjacent = new Node[] { new Node(), new Node() { Adjacent = new Node[] { new Node() { Adjacent = new Node[] { new Node() } }, new Node() } }, new Node() };
+
+            // Act
+            BFS.Search(target);
+
+            // Assert
+            AssertNode(target);
+        }
+
+        [Test]
+        public void Search_should_reach_if_node_zero()
+        {
+            // Arrange
+            var target = new Node();
+            target.Adjacent = null;
+
+            // Act
+            BFS.Search(target);
+
+            // Assert
+            AssertNode(target);
+        }
+
+        [Test]
+        public void Search_should_reach_if_node_one()
+        {
+            // Arrange
+            var target = new Node();
+            target.Adjacent = new Node[] { new Node() };
+
+            // Act
+            BFS.Search(target);
+
+            // Assert
+            AssertNode(target);
+        }
+
+        private void AssertNode(Node node)
+        {
+            Assert.IsTrue(node.Visited);
+            if (node.Adjacent != null)
+            { 
+                foreach (var n in node.Adjacent)
+                {
+                    Assert.IsTrue(n.Visited);
+                    AssertNode(n);
+                }
+            }
+        }
+    }
+}
---
+++ DFS/BFS.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFS
+{
+    public class BFS
+    {
+        public static void Search( Node root )
+        {
+            var queue = new Queue<Node>();
+            root.Visited = true;
+            Visit(root);
+            queue.Enqueue(root);
+
+            while(queue.Count != 0)
+            {
+                var current_node = queue.Dequeue();
+                if (current_node.Adjacent != null)
+                { 
+                    foreach (var node in current_node.Adjacent)
+                    {
+                        if(!node.Visited)
+                        {
+                            Visit(node);
+                            node.Visited = true;
+                            queue.Enqueue(node);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void Visit(Node root)
+        {
+            // nop
+        }
+    }
+}
