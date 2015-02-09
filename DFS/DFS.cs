b92d2ce6de5a09626b26c5c7f13ca8f47a1acbe1
Koki <u2daijibrother@gmail.com>
2015-02-10 05:24:46 +0900

    create product.

---
+++ DFS/DFS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFS
+{
+    public class DFS
+    {
+        public void Search( Node root )
+        {
+            if(root == null)
+            {
+                return;
+            }
+            Visit(root);
+            root.Visited = true;
+            foreach (var node in root.Adjacent)
+            {
+                if(!node.Visited)
+                {
+                    Search(node);
+                }
+            }
+        }
+
+        private void Visit(Node root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DFS/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DFS
+{
+    public class Node
+    {
+        public bool Visited { get; set; }
+
+        public Node[] Adjacent { get; set; }
+    }
+}
